65c71418de56461e214e93900e42add6d7962fd0
Marcus Venghaus <marcus.venghaus@inklammern.de>
2021-04-29 13:55:55 +0200

    moved "widget_name" functionality to own module "m2-module-widget-ext-name"

--- src/view/adminhtml/requirejs-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    map: {
-        '*': {
-            'Magento_Backend/mage/adminhtml/wysiwyg/tiny_mce/plugins/magentowidget/editor_plugin': 'Inkl_WidgetExtParameter/js/editor_plugin'
-        }
-    }
-};
